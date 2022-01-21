eb8697f031c0aa86e7c2dde4a866a6d873f52315
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-21 21:27:38 +0200

    Initial Commit

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Animated Progress Bar
+//
+//  Created by Сергей on 21.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
