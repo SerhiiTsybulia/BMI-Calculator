014f5aa4e07cc1aa9192284420ca90377fe96156
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-21 22:17:26 +0200

    linked sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weigthSlider: UISlider!
+    @IBOutlet weak var heightLable: UILabel!
+    @IBOutlet weak var weightLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLable.text = "\(height)m"
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        weightLable.text = "\(weight)Kg"
+    }
+    
 
 }
 
