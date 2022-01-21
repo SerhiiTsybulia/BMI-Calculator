0c16ce4f8f8bb551ae8a191133cd2b6dabbed066
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-21 21:38:23 +0200

    Initial commit

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -1,8 +1,9 @@
 //
-//  AppDelegate.swift
-//  Animated Progress Bar
+//  ViewController.swift
+//  BMI Calculator
 //
 //  Created by Сергей on 21.01.2022.
+//  Copyright © 2022 STDevelopment. All rights reserved.
 //
 
 import UIKit
